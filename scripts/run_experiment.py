36fedb7db393b6784305312f25ad231d8baed5cd
Haemanth V <haemanth10@gmail.com>
2024-04-29 13:47:24 +0530

    Added filters with full entanglement

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -1,4 +1,4 @@
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import json
 from Filters import Filters
 from Data_load import data_load
@@ -9,69 +9,76 @@ from utils import *
 
 import time
 
-## Import the images 
+# Import the images
 data = data_load()
 train_images, train_labels, test_images, test_labels = data.data_mnist()
 
-types ={
+types = {
     0: "geometrical",
     1: "classical_w_pooling",
     2: "quantum_random",
     3: "quantum_cnot",
-    4: "classical_wo_pooling"
+    4: "classical_wo_pooling",
+    5: "full",
+    6: "full_asc"
 }
 
 if data.PREPROCESS == True:
 
     for t, type in types.items():
         start_time = time.time()
-        filtered_train_images = apply_filter(train_images, t, data.n_channels, data.n_layers)
-        filtered_test_images = apply_filter(test_images, t, data.n_channels, data.n_layers)
+        filtered_train_images = apply_filter(
+            train_images, t, data.n_channels, data.n_layers)
+        filtered_test_images = apply_filter(
+            test_images, t, data.n_channels, data.n_layers)
         end_time = time.time()
         # Save pre-processed images
-        np.save(data.SAVE_PATH + "filtered_train_images_{}.npy".format(type), filtered_train_images)
-        np.save(data.SAVE_PATH + "filtered_test_images_{}.npy".format(type), filtered_test_images)
-        
+        np.save(data.SAVE_PATH + "filtered_train_images_{}.npy".format(type),
+                filtered_train_images)
+        np.save(data.SAVE_PATH +
+                "filtered_test_images_{}.npy".format(type), filtered_test_images)
+
         # Calculate the time taken
         time_taken = end_time - start_time
         # Save time taken to a text file
         with open(data.SAVE_PATH + "time_for_{}.txt".format(type), "w") as file:
-            file.write("Time taken for filtering data: {} seconds".format(time_taken))
+            file.write(
+                "Time taken for filtering data: {} seconds".format(time_taken))
 
-
-    
 
 # Training !!!!
-n_classes = data.n_classes # 10 for mnist dataset
+n_classes = data.n_classes  # 10 for mnist dataset
 
 for t, type in types.items():
     # Instantiate a Model class
-    my_model = Model(n_classes = data.n_classes) # 10 for mnist dataset)
+    my_model = Model(n_classes=data.n_classes)  # 10 for mnist dataset)
     # Compile the model
     my_model.compile_model()
-    
-    train_images = load_data(data.SAVE_PATH + "filtered_train_images_{}.npy".format(type))
-    test_images = load_data(data.SAVE_PATH + "filtered_test_images_{}.npy".format(type))
-    
+
+    train_images = load_data(
+        data.SAVE_PATH + "filtered_train_images_{}.npy".format(type))
+    test_images = load_data(
+        data.SAVE_PATH + "filtered_test_images_{}.npy".format(type))
+
     # in case if you need to choose chunks of the filtered data, this can be done below here
     # ........
 
-    
     start_time = time.time()
     # Train and validate the model (passing train_images and train_labels, test_images, test_labels)
     model_history = my_model.train_model(train_images,
-                                            train_labels,
-                                            test_images,
-                                            test_labels,
-                                            batch_size=data.batch_size,
-                                            epochs = data.n_epochs)
+                                         train_labels,
+                                         test_images,
+                                         test_labels,
+                                         batch_size=data.batch_size,
+                                         epochs=data.n_epochs)
     end_time = time.time()
     time_taken = end_time - start_time
-        # Save time taken to a text file
+    # Save time taken to a text file
     with open(data.SAVE_PATH + "time_for_{}.txt".format(type), "a") as file:
-        file.write("\nTime taken for training model: {} seconds".format(time_taken))
+        file.write(
+            "\nTime taken for training model: {} seconds".format(time_taken))
     # save model history
-    with open(data.SAVE_PATH + "model_{}.json".format(type),'w') as json_file:
+    with open(data.SAVE_PATH + "model_{}.json".format(type), 'w') as json_file:
         json.dump(model_history.history, json_file)
 
 

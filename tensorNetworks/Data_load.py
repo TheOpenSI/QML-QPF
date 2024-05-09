d15761d5b01a6b66215506ee35fde5e54923bc49
hosen20 <h.shiri@st.ul.edu.lb>
2024-05-10 00:18:34 +0300

    added the solution for 2 classes classification problem

--- tensorNetworks/Data_load.py
+++ tensorNetworks/Data_load.py
@@ -4,6 +4,7 @@ from tensorflow import keras
 from pennylane import numpy as np
 import tensorflow as tf
 
+
 class data_load():
 
     def __init__(self,):
@@ -15,39 +16,44 @@ class data_load():
         self.n_layers = config['RNDM_LAYERS']    # Number of random layers
         self.n_train = config['TRAIN_IMAGES']    # Size of the train dataset
         self.n_test = config['TEST_IMAGES']     # Size of the test dataset
-        self.n_channels = config['CHANNELS'] # Number of channels
-        self.is_reduced = config['DATA_REDUCTION'] # its 0 (false) or 1 (True)
-        self.reduction_ratio = config['REDUCTION_RATIO'] # its between 0 and 1
+        self.n_channels = config['CHANNELS']  # Number of channels
+        self.is_reduced = config['DATA_REDUCTION']  # its 0 (false) or 1 (True)
+        self.reduction_ratio = config['REDUCTION_RATIO']  # its between 0 and 1
 
         self.SAVE_PATH = config['SAVE_PATH']  # Data saving folder
-        self.PREPROCESS = config['PREPROCESS']           # If False, skip quantum processing and load data from SAVE_PATH
-        
-        self.n_classes = config["CLASSES"] # represents number of classes in the data.
-    
-        
-        self.FILTER_TYPE = config["FILTER_TYPE"] # O means geometrical filter (rotation&trace), 1 means classical filter (conv&max_pooling), 2 means quantum (random), 3 means quantum (cnot) 
-        
+        # If False, skip quantum processing and load data from SAVE_PATH
+        self.PREPROCESS = config['PREPROCESS']
+
+        # represents number of classes in the data.
+        self.n_classes = config["CLASSES"]
+
+        # O means geometrical filter (rotation&trace), 1 means classical filter (conv&max_pooling), 2 means quantum (random), 3 means quantum (cnot)
+        self.FILTER_TYPE = config["FILTER_TYPE"]
+
         self.batch_size = config["BATCH_SIZE"]
-           
+
         return
-    
+
     def data_mnist(self,):
 
-        ## Load the data set
+        # Load the data set
         mnist_dataset = keras.datasets.mnist
-        (train_images, train_labels), (test_images, test_labels) = mnist_dataset.load_data()
+        (train_images, train_labels), (test_images,
+                                       test_labels) = mnist_dataset.load_data()
 
         if self.is_reduced:
             # Reduce dataset size
-            train_images, train_labels, test_images, test_labels = choose_samples(train_images, train_labels, test_images, test_labels, self.reduction_ratio*100)
+            train_images, train_labels, test_images, test_labels = choose_samples(
+                train_images, train_labels, test_images, test_labels, self.reduction_ratio)
 
         # Normalize pixel values within 0 and 1
         train_images = train_images / 255
         test_images = test_images / 255
 
         # Add extra dimension for convolution channels
-        train_images = np.array(train_images[..., tf.newaxis], requires_grad=False)
-        test_images = np.array(test_images[..., tf.newaxis], requires_grad=False)
+        train_images = np.array(
+            train_images[..., tf.newaxis], requires_grad=False)
+        test_images = np.array(
+            test_images[..., tf.newaxis], requires_grad=False)
 
         return train_images, train_labels, test_images, test_labels
-
--- tensorNetworks/utils.py
+++ tensorNetworks/utils.py
@@ -4,7 +4,9 @@ import pennylane as qml
 
 from Filters import Filters
 
-kernel = np.random.rand(2, 2) # we can create a class including the different type of kernels, if required
+# we can create a class including the different type of kernels, if required
+kernel = np.random.rand(2, 2)
+
 
 def choose_samples(train_images, train_labels, test_images, test_labels, reduction_ratio):
     """
@@ -34,16 +36,19 @@ def choose_samples(train_images, train_labels, test_images, test_labels, reducti
 
     return train_images_selected, train_labels_selected, test_images_selected, test_labels_selected
 
+
 def read_configurations(config_file):
     with open(config_file, 'r') as f:
         config = json.load(f)
     return config
 
-## apply the selected filter on an image
-def filter(image, type, n_channels, n_layers):
+# apply the selected filter on an image
+
+
+def filter(image, type, n_channels, n_layers, meas_last):
     f = Filters(image, n_channels, n_layers)
     if type == 0:
-        filtered_image  = f.geometry_filter()
+        filtered_image = f.geometry_filter()
     elif type == 1:
         filtered_image = f.convolution_filter(kernel=kernel)
     elif type == 2:
@@ -52,22 +57,34 @@ def filter(image, type, n_channels, n_layers):
         filtered_image = f.quantum_conv_filter("cnot")
     elif type == 4:
         filtered_image = f.convolution_filter(kernel=kernel, pooling=False)
+    elif type == 5:
+        # measure only 1 qubit since all qubits are entangled (measures all qubits if meas_last is not set to True)
+        filtered_image = f.quantum_conv_filter("full", meas_last)
+    elif type == 6:
+        # measure only 1 qubit since all qubits are entangled
+        filtered_image = f.quantum_conv_filter("full_asc", meas_last)
+    # elif type == 7:
+    #     filtered_image = f.quantum_conv_filter("no_cnot")
     return filtered_image
 
-## apply the filters for all images
-def apply_filter(images, type, n_channels, n_layers):
+
+# apply the filters for all images
+# meas_last is used to measure only the last qubit whenever there is full entanglement in the filters
+def apply_filter(images, type, n_channels, n_layers, meas_last=True):
     filtered_images = []
     print("fitlered pre-processing of images, with type = {}".format(type))
-    
+
     for idx, img in enumerate(images):
         print("{}/{}        ".format(idx + 1, np.shape(images)[0]), end="\r")
-        filtered_images.append(filter(img, type, n_channels, n_layers))
-    
+        filtered_images.append(
+            filter(img, type, n_channels, n_layers, meas_last))
+
     filtered_images = np.asarray(filtered_images)
 
-    return(filtered_images)
+    return (filtered_images)
+
 
 def load_data(image_file):
-        """Loads the training data from .npy files."""
-        train_images = np.load(image_file)
-        return train_images
+    """Loads the training data from .npy files."""
+    train_images = np.load(image_file)
+    return train_images

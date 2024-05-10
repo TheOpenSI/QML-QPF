6ef2cbe9cd488a00002d07e8967e7399ae556255
Carlos Kuhn <31604991+carlosclaiton@users.noreply.github.com>
2024-05-10 03:23:57 +0000

    adding a prediction and confusion matrix fucntion

--- scripts/Model.py
+++ scripts/Model.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 from tensorflow import keras
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Model:
     def __init__(self, n_classes):
@@ -29,3 +32,31 @@ class Model:
                        batch_size=batch_size,
                        epochs=epochs,
                        verbose=2)
+        
+    def prediction(self, my_model, test_images, test_labels):
+        """Use this function to make prediction and return the data ready for generation of confusion matrix"""
+        
+        predicted = my_model.predict(test_images)
+        actual = tf.stack(test_labels, axis=0)
+        predicted = tf.concat(predicted, axis=0)
+        predicted = tf.argmax(predicted, axis=1)
+        labels = np.unique(test_labels)
+
+        return actual, predicted, labels
+        
+        
+    def plot_confusion_matrix(self, actual, predicted, labels, ds_type):
+        """
+        This function  is used to plot the confusion matrix 
+        """
+        cm = tf.math.confusion_matrix(actual, predicted)
+        ax = sns.heatmap(cm, annot=True, fmt='g')
+        sns.set(rc={'figure.figsize':(12, 12)})
+        sns.set(font_scale=1.4)
+        ax.set_title('Confusion matrix of Handwritten MNIST data set ' + ds_type)
+        ax.set_xlabel('Predicted Action')
+        ax.set_ylabel('Actual Action')
+        plt.xticks(rotation=90)
+        plt.yticks(rotation=0)
+        ax.xaxis.set_ticklabels(labels)
+        ax.yaxis.set_ticklabels(labels)
